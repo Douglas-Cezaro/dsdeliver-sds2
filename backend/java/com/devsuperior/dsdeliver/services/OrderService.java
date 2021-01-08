5b449f0ef56467f296a3ebb158b6370b0509ef0a
douglas-cezaro <cezarodouglas@gmail.com>
2021-01-07 21:08:04 -0300

    POST /orders

--- backend/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
@@ -24,6 +32,25 @@ public class OrderService {
 
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		
+		//Instanciando um order
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLatitude(), 
+				Instant.now(), OrderStatus.PENDING);
+		
+		// Fazer um for com os produtos que vieram do dto, e adicionar a referencia no order_products
+		for(ProductDTO p: dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		
+		order = repository.save(order);
+		
+		return new OrderDTO(order);
+		
 	}
 
 }
